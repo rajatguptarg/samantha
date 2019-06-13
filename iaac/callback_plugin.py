5c16d3e216bfd8ce4767a91e2730ca1adab65e55
Rajat Gupta <rajat.gupta712@gmail.com>
2019-06-13 13:57:52 +0200

    #02: Added callback plugin for ansible

---
+++ iaac/callback_plugin.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+Author: Rajat Gupta
+Description:
+"""
+
+import json
+from ansible.plugins.callback import CallbackBase
+
+
+__all__ = ['ResultCallback']
+
+
+class ResultCallback(CallbackBase):
+    """
+    A sample callback plugin used for performing an action as results come in
+
+    If you want to collect all results into a single object for processing at
+    the end of the execution, look into utilizing the ``json`` callback plugin
+    or writing your own custom callback plugin
+    """
+    def v2_runner_on_ok(self, result, **kwargs):
+        """
+        Print a json representation of the result
+
+        This method could store the result in an instance attribute for retrieval later
+        """
+        host = result._host
+        print(json.dumps({host.name: result._result}, indent=4))
